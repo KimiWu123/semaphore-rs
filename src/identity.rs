06c288489d2b36f66c276e604b9813265a33872b
KimiWu <leonhartwu@gmail.com>
2025-01-26 14:24:17 +0800

    lint: fix lint

--- src/identity.rs
+++ src/identity.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use ruint::aliases::U256;
 
+#[allow(dead_code)]
 pub struct Identity {
     private_key: Vec<u8>,
     secret_scalar: U256,
@@ -8,16 +9,17 @@ pub struct Identity {
     commitment: U256,
 }
 
+#[allow(dead_code)]
 impl Identity {
-    fn sign_message(&self, message: &[u8]) -> Result<()> {
+    fn sign_message(&self, _message: &[u8]) -> Result<()> {
         unimplemented!()
     }
 
-    fn verify_signature(message: &[u8], signature: &[u8], public_key: &[u8]) -> Result<()> {
+    fn verify_signature(_message: &[u8], _signature: &[u8], _public_key: &[u8]) -> Result<()> {
         unimplemented!()
     }
 
-    fn generate_commitment(public_key: &[u8]) -> Result<()> {
+    fn generate_commitment(_public_key: &[u8]) -> Result<()> {
         unimplemented!()
     }
 }
